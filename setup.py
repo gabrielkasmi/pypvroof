06e7891befed0c2e7a5e8c771a78e48a61ff8689
gabrielkasmi <gabriel.kasmi@yahoo.com>
2025-04-23 16:29:21 +0200

    cleaned cached

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="pypvroof",
     version="0.1.0",
-    author="Gabriel",
-    author_email="your.email@example.com",
+    author="Gabriel Kasmi",
+    author_email="gabriel.kasmi@minesparis.psl.eu",
     description="A Python package for PV roof mapping and analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/pypvroof",
+    url="https://github.com/gabrielkasmi/pypvroof",
     packages=find_packages(),
     package_data={
         "pypvroof": ["data/*.json"],
     },
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -29,5 +30,8 @@ setup(
         "matplotlib",
         "geojson",
         "tqdm",
+        "GDAL>=3.6.2",
+        "shapely",
+        "rasterio",
     ],
 ) 
